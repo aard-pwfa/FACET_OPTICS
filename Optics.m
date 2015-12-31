98775cab9a0aad4764bb71986e74c1c7528c8795
Spencer <sgess@AR-PC91223.local>
2015-12-31 11:41:19 +1100

    added axiramp

--- Optics.m
+++ Optics.m
@@ -6,24 +6,30 @@ end
 
 % Optic parameters
 if strcmp(input.type,'axilens')
-    k0 = 2*pi/input.lambda;
-    R  = input.R;
-    f0 = input.f0;
-    Dz = input.Dz;
+    k0  = 2*pi/input.lambda;
+    R   = input.R;
+    f0  = input.f0;
+    Dz  = input.Dz;
     PSI = k0*R^2/(2*Dz)*log(f0+Dz/(R^2)*rr.^2);
 elseif strcmp(input.type,'axicon')
     gamma = 0.46*input.ax_ang*pi/180;
     kp    = 2*pi*sin(gamma)/input.lambda;
-    PSI = kp*rr;
+    PSI   = kp*rr;
 elseif strcmp(input.type,'kinoform')
     gamma = input.gamma;
     kp    = 2*pi*sin(gamma)/input.lambda;
     m     = input.m;
-    PSI = kp*rr+m*phi;
+    PSI   = kp*rr+m*phi;
 elseif strcmp(input.type,'lens')
-    k0 = 2*pi/input.lambda;
-    f  = input.f;
+    k0  = 2*pi/input.lambda;
+    f   = input.f;
     PSI = k0*rr.^2/(2*f);
+elseif strcmp(input.type,'axiramp')
+    k0  = 2*pi/input.lambda;
+    R   = input.R; 
+    f0  = input.f0;
+    Dz  = input.Dz;
+    PSI = k0*(rr*R/Dz-f0*R^2/Dz^2*log(1+Dz*rr/(R*f0)));
 else
     disp('Bad optic type');
 end
---
+++ axiramp_deck.m
@@ -0,0 +1,54 @@
+%% Parameter Deck
+
+% Computation parameters
+input.n_pts  = 2048;   % number of pixels
+input.res    = 4.00;   % resolution in image plane (microns)
+input.nz     = 101;    % number of points in z
+input.zmin   = 2.50e6; % starting z position (microns)
+input.zmax   = 5.00e6; % ending z position (microns)
+
+% Ti:Saph parameters
+input.lambda = 0.800;   % wavelength (microns)
+input.t0     = 100e-15; % pulse width (s)
+input.e0     = 300e-3;  % pulse energy (J)
+input.eta    = 0.49;    % transmission efficieny
+input.r0     = 2.0;     % laser radius (cm)
+
+% Optic parameters
+input.type   = 'axiramp'; % can choose from 'axicon', 'axilens', and 'kinoform'
+input.R      = 20.0e3;    % illuminated radius of mask (microns)
+input.f0     = 3e6;       % distance to axilens focus (microns) [if type==axilens or axiramp]
+input.Dz     = 1e6;       % length of axilens focus (microns) [if type==axilens or axiramp]
+input.ax_ang = 0.75;      % axicon angle (degrees) [if type==axicon]
+input.gamma  = 4.4e-3;    % convergence angle (radians) [if type==kinoform]
+input.m      = 7;         % bessel order [if type==kinoform]
+input.f      = 4e6;       % focal length of lens [if type==lens]
+
+% Illumination parameters
+input.illum  = 'tophat'; % options are 'tophat' which is an order 30 super gaussian, . . . 
+                         % 'gaussian', 'mask' which has a hard edge, and 'flat' which is an infinite beam
+input.sigma  = 20.0e3;   % sigma of gaussian/super gaussian or radius of mask (microns)
+
+% Aberation parameters
+input.zern_amp = 0;   % amplitude of aberation
+input.zern_m   = 0;   % add beam aberations with Zernike polynomials, for example . . .
+input.zern_n   = 0;   % m = 2, n = 2 is a vertical astigmatism. 0,0 is no aberation. For more, see . . .
+                      % https://en.wikipedia.org/wiki/Zernike_polynomials#Zernike_polynomials
+
+% Other stuff
+input.thresh = 2E14; % ADK Threshold (W/cm^2)
+
+%% Calculate output intensity
+
+output = Fresnel_Prop(input);
+
+%% Plot data
+save_dir = 'plots/axiramp/';
+save_bool = 1;
+plot_window = 512;
+
+plot_fun(input,output,'3D',plot_window,1,save_bool,save_dir);
+plot_fun(input,output,'IZ',plot_window,2,save_bool,save_dir);
+plot_fun(input,output,'IR',plot_window,3,save_bool,save_dir);
+plot_fun(input,output,'IntThresh',plot_window,4,save_bool,save_dir);
+plot_fun(input,output,'IntContour',plot_window,5,save_bool,save_dir);
